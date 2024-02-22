23e53daf54fffef166956044e49c540d6944f056
KEVIN CARLOS LOPEZ GONZALEZ <10043042@horusur.corp>
2024-02-21 21:42:51 -0600

    update_dependencies_pom_eurekaClient

--- src/main/java/com/nivektion/bookingmicroservice/BookingMicroserviceApplication.java
+++ src/main/java/com/nivektion/bookingmicroservice/BookingMicroserviceApplication.java
@@ -2,8 +2,10 @@ package com.nivektion.bookingmicroservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class BookingMicroserviceApplication {
 
 	public static void main(String[] args) {
